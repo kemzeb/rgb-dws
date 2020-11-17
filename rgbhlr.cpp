f3d2f3c5d68bed9d2148921cc529865c865cd74d
Kemal Zebari <kemalzebra@gmail.com>
2020-11-17 03:30:45 -0800

    Add source file for rgbhlr.cpp

---
+++ rgbhlr.cpp
@@ -0,0 +1,20 @@
+#include "rgbhlr.h"
+
+//////////////////////////////////////////////////
+// RGB methods
+//////////////////////////////////////////////////
+
+// Constructors
+rgb_dws::RGB::RGB() : red(0), green(0), blue(0) 
+{}
+
+rgb_dws::RGB::RGB(short r, short g, short b) : red(r), green(g), blue(b) 
+{}
+
+//////////////////////////////////////////////////
+// RGBCalc methods
+//////////////////////////////////////////////////
+
+// Constructors
+rgb_dws::RGBCalc::RGBCalc(double r, double g, double b) : red(r), green(g), blue(b)
+{}
