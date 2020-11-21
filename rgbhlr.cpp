c1bf5a73c0f1924fab0491d664029e7d6e939015
Kemal Zebari <kemalzebra@gmail.com>
2020-11-20 21:44:52 -0800

    Add copy constructor

--- rgbhlr.cpp
+++ rgbhlr.cpp
@@ -5,10 +5,10 @@
 //////////////////////////////////////////////////
 
 // Constructors
-rgb_dws::RGB::RGB() : red(0), green(0), blue(0) 
+rgb_dws:: RGB::RGB() : red(0), green(0), blue(0) 
 {}
 
-rgb_dws::RGB::RGB(short r, short g, short b) : red(r), green(g), blue(b) 
+rgb_dws:: RGB::RGB(short r, short g, short b) : red(r), green(g), blue(b) 
 {}
 
 //////////////////////////////////////////////////
@@ -16,5 +16,12 @@ rgb_dws::RGB::RGB(short r, short g, short b) : red(r), green(g), blue(b)
 //////////////////////////////////////////////////
 
 // Constructors
-rgb_dws::RGBCalc::RGBCalc(double r, double g, double b) : red(r), green(g), blue(b)
+rgb_dws:: RGBCalc::RGBCalc() : red(0), green(0), blue(0)
 {}
+
+rgb_dws:: RGBCalc::RGBCalc(double r, double g, double b) : red(r), green(g), blue(b)
+{}
+
+rgb_dws:: RGBCalc::RGBCalc(const rgb_dws::RGBCalc& c) : red(c.red), green(c.green), blue(c.blue)
+{}
+	
